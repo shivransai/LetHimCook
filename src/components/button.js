dcefd00cf6172f4c8e6d0409ec42d304d5260cc9
Shivran Muralidhar <shivran.sai@macbook-pro.lan>
2023-06-13 00:22:00 -0400

    updating layout for phones + improve aesthetics

--- src/components/button.js
+++ src/components/button.js
@@ -4,9 +4,11 @@ import "./component.css"
 const Button = ({ onClick, children, isLoading }) => {
 
   return (
+    <div className='button-div'>
     <button onClick={onClick} className={isLoading ? 'button-disabled' : 'button-enabled'} disabled={isLoading}>
       {children}
     </button>
+    </div>
   );
 };
 
